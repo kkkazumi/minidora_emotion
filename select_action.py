72ffe5d48b6070a9dba2254f8bbf672d3242ea19
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2017-10-11 23:06:43 +0900

    ittan commit

--- select_action.py
+++ select_action.py
@@ -1,1 +1,19 @@
+# -*- encoding: UTF-8 -*-
+
+
+
+if __name__ == "__main__":
+
+    situation_max=10
+
+    print("input number of situations")
+    wait_key=raw_input()
+    #if wait_key>situation_max:
+    #    message="input number 0 to %s, again" % situation_max
+    #    print(message) 
+    if wait_key<situation_max:
+        situation_num=wait_key
+
+    for test_action in range(1, 5):
+        action_num=test_action
 
