b971b91793e56681ef317f6932c29767bd1808c7
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2017-10-12 10:13:41 +0900

    add some code

--- select_action.py
+++ select_action.py
@@ -2,18 +2,25 @@
 
 
 
+
 if __name__ == "__main__":
 
-    situation_max=10
+    situation_number = 3 # three patterns of situations
+    action_number = 5 #five patterns of robot's actions
+
+
+    N_layer = [ 3,5,1 ]
+
+    NN = Neural_Network( N_layer,teaching_num )
 
     print("input number of situations")
     wait_key=raw_input()
     #if wait_key>situation_max:
     #    message="input number 0 to %s, again" % situation_max
     #    print(message) 
-    if wait_key<situation_max:
-        situation_num=wait_key
+    situation_num=wait_key
 
     for test_action in range(1, 5):
+
         action_num=test_action
 
